39f69e7b5fdee6cb4f069a39edf456b6d4e6ac92
Nik <weinni2000@gmail.com>
2026-05-19 10:15:53 +0200

    Test

--- date_form_advanced_widget/migrations/19.0.1.0.0/post-migrate.py
+++
@@ -1,6 +0,0 @@
-from odoo import SUPERUSER_ID, api
-
-
-def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env["res.country"].action_set_default_preferred_guest_registration()
